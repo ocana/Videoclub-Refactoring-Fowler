5e986be92374714decfcd3aba127ce984bcc61b3
Miguel Ocaña <miguel.ocana@gmail.com>
2015-01-27 14:11:01 +0100

    Move Method GetCharge from Movie to Price

--- Refactoring/Movie.cs
+++ Refactoring/Movie.cs
@@ -41,30 +41,7 @@
 
         public double GetCharge(int daysRented)
         {
-            double result = 0;
-
-            switch (PriceCode)
-            {
-                case REGULAR:
-                    result += 2;
-                    if (daysRented > 2)
-                    {
-                        result += (daysRented - 2) * 1.5;
-                    }
-                    break;
-                case NEW_RELEASE:
-                    result += daysRented * 3;
-                    break;
-                case CHILDRENS:
-                    result += 1.5;
-                    if (daysRented > 3)
-                    {
-                        result += (daysRented - 3) * 1.5;
-                    }
-                    break;
-            }
-
-            return result;
+            return _price.GetCharge(daysRented);
         }
 
         public int GetFrequentRenterPoints(int daysRented)
