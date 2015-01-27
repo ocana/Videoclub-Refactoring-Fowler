21b878115bee29776979108939f50a571017a5ba
Miguel Ocaña <miguel.ocana@gmail.com>
2015-01-27 12:30:21 +0100

    Move Method GetCharge to the Movie class
    Users are clamoring to change (probably add) the classifications of the
    movies, so we prefer to calculate the charge within the movie.

--- Refactoring/Movie.cs
+++ Refactoring/Movie.cs
@@ -15,5 +15,33 @@
         public int PriceCode { get; set; }
 
         public string Title { get; private set; }
+
+        public double GetCharge(int daysRented)
+        {
+            double result = 0;
+
+            switch (PriceCode)
+            {
+                case REGULAR:
+                    result += 2;
+                    if (daysRented > 2)
+                    {
+                        result += (daysRented - 2) * 1.5;
+                    }
+                    break;
+                case NEW_RELEASE:
+                    result += daysRented * 3;
+                    break;
+                case CHILDRENS:
+                    result += 1.5;
+                    if (daysRented > 3)
+                    {
+                        result += (daysRented - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            return result;
+        }
     }
 }
